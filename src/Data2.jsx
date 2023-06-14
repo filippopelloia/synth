5dce481c310884900f06f48bc3a5da8b407291e2
Dev1 - filip <pelloiafilippo@live.com>
2023-06-14 09:38:03 +0200

    Second commit

--- src/Data2.jsx
+++ src/Data2.jsx
@@ -16,6 +16,13 @@ const dialoghi2 = [
     {id: 3, url: `${foto4}`, character: `${char3}`, content: "TU: \"Chi l'avrebbe mai detto che da *INSERISCI PASSATO* sarei finito a fare il contadino in una colonia extra-mon...\""},
     {id: 4, url: `${foto5}`, character: `${char3}`, content: "TU: \"Hey, ma che..?\""},
     {id: 5, url: `${foto5}`, character: `${char3}`, content: "CRAAAAAAAAAAAAAASSHHH!!!"},
+    {id: 6, url: `${foto5}`, character: `${char3}`, content: "TU: \"Aaaaaahhhh...!!!\"  "},
+    {id: 7, url: `${foto5}`, character: `${char3}`, content: "TU: \"...\"  "},
+    {id: 8, url: `${foto5}`, character: `${char3}`, content: "TU: \"E' uno scherzo? Non puo essere...\" "},
+    {id: 9, url: `${foto5}`, character: `${char2}`, content: "???: \"Ma benvenuto... o meglio, BENTORNATO.\" "},
+    {id: 10, url: `${foto5}`, character: `${char2}`, content: "???: \"E' passato un bel pò dalla tua ultima visita qui, caro *NOME*\" "},
+    {id: 11, url: `${foto5}`, character: `${char3}`, content: "TU: \"Che ci faccio di nuovo sulla Terra? Fino ad un attimo fa ero su Solara!\" "},
+
     {id: 25, url: `${foto6}`, character: `${char3}`, content: "Questo è il contenuto numero 25. Ce l'hai fatta!!!"}
 ]
 
